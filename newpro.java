f1c2f50aaf852c4e442759e3d2f3706106ed4c60
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-04-23 13:09:55 +0530

    added new line

--- newpro.java
+++ newpro.java
@@ -3,6 +3,7 @@ public class newpro {
 public static void main(String[] args) {
     System.out.println(" boom");
     System.out.println(" Hello everyone ");
+    System.out.println(" keep going bro ");
 }
     
 }
