410d77abd784fcb49fadee1513b3fab3d792202f
Sandeep Dev <105205047+sandeep-gitdev@users.noreply.github.com>
2024-04-23 13:02:46 +0530

    newpro.java updated

--- newpro.java
+++ newpro.java
@@ -1,6 +1,7 @@
 public class newpro {
 public static void main(String[] args) {
     System.out.println(" boom");
+    System.out.println(" this line is here to show how merging works. ");
 }
     
 }
